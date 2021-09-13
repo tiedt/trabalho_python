0fb299ae7c7e2f12d86ea5ac83c4f461c1259a88
Rennã-Tiedt-FAEL <renna.tiedt@fael.edu.br>
2021-09-13 20:51:45 -0300

    criação da sujeira

--- ExemploProfessora.py
+++ ExemploProfessora.py
@@ -1,7 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import random
+import matplotlib.collections as collections
+from random import randint
+import sys
 
+TamanhoMatriz = 6
+MatrixSujeira = []
+MaximoSujeira = 7
 
 # Função que exibe o ambiente na tela
 def exibir(I):
@@ -11,15 +16,50 @@ def exibir(I):
     # Altera o esquema de cores do ambiente
     plt.imshow(I, 'gray')
     plt.nipy_spectral()
-
+    criarSujeira(MaximoSujeira, MatrixSujeira)
     # Coloca o agente no ambiente
     plt.plot([posAPAy], [posAPAx], marker='o', color='r', ls='')
-
+    disporSujeira(posAPAx,posAPAy)
     plt.show(block=False)
 
     # Pausa a execução do código por 0.5 segundos para facilitar a visualização
     plt.pause(10000000000.5)
     plt.clf()
+
+def criarSujeira(MaximoSujeira, MatrixSujeira):
+    x = MatrixSujeira
+    if MaximoSujeira < 1:
+        return
+
+    CoordenadaSujeiraX = randint(2, TamanhoMatriz - 1)
+    CoordenadaSujeiraY = randint(2, TamanhoMatriz - 1)
+    CoordenadaSujeiraX = CoordenadaSujeiraX - 1
+    CoordenadaSujeiraY = CoordenadaSujeiraY - 1
+    coordenadas = [CoordenadaSujeiraX, CoordenadaSujeiraY]
+
+    if coordenadas in MatrixSujeira:
+        criarSujeira(MaximoSujeira, x)
+    else:
+        if CoordenadaSujeiraX == posAPAx and CoordenadaSujeiraY == posAPAy:
+            criarSujeira(MaximoSujeira, x)
+        else:
+            MatrixSujeira.append(coordenadas)
+            MaximoSujeira = MaximoSujeira - 1
+            criarSujeira(MaximoSujeira, x)
+
+
+def disporSujeira(posAPAx,posAPAy):
+    k = 0
+    for i in MatrixSujeira:
+        TempX = ''
+        TempY = ''
+        for j in MatrixSujeira[k]:
+            if (TempX == ''):
+                TempX = j
+            else:
+                TempY = j
+                plt.plot(float(TempX), float(TempY), 'ow', markersize=10)
+                k += 1
 
 
 
